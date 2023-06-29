47d9045d6203058f6f873fcb4f3b1517be4b567d
jero esguerra <jeroesguerra@gmail.com>
2023-06-28 17:30:28 -0700

    first commit

---
+++ db/migrate/20230626000654_create_feedbacks.rb
@@ -0,0 +1,14 @@
+class CreateFeedbacks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :feedbacks do |t|
+      t.string :text
+      t.string :recipient_handle
+      t.string :tweet_url
+      t.string :tweet_id
+      t.boolean :sent_successfully, default: false
+
+
+      t.timestamps
+    end
+  end
+end
